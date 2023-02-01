a8d6f4ad1c9a0ce6bef46f14ae840d2bc7dddc19
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2023-02-01 08:49:22 +0100

    style banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from "../components/Header"
 import Image from 'next/image'
+import Banner from './../components/Banner';
 
 const Home: NextPage = () => {
   return (
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+      <Banner />
 
      
     </div>
