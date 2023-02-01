1f955cdb946332eaaa3ca66ada8e92c344148d87
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2023-02-01 15:50:14 +0100

    style the middle card

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import bg from '../images/pexels-andrea-davis-11786266.jpg'
+import bg from '../public/md1.jpg'
 const Banner = () => {
   return (
     <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[500px] 2xl:h-[700px]'>
@@ -12,7 +12,6 @@ const Banner = () => {
       />
       <div className='absolute top-1/2 w-full text-center'>
         <p className='text-sm sm:text-lg'>Confused of where to go? We've got you</p>
-
         <button className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full my-3 hover:shadow-xl active:scale-90 transition duration-150 font-bold'>I'm Flexible</button>
       </div>
     </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,10 @@
-
-import {useEffect} from 'react'
 import Head from 'next/head'
 import Header from "../components/Header"
-import Image from 'next/image'
 import Banner from '../components/Banner';
 import mydata from '../data.json'
+import middleData from '../middleCard.json'
 import SmallCard from './../components/SmallCard';
+import MiddleCard from './../components/MiddleCard';
 
 const Home = () => {
  
@@ -32,6 +31,12 @@ const Home = () => {
 
         <section>
           <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+              {middleData.map(({img,title}) => (
+              <MiddleCard key={img} img={img} title={title} />
+            ))}
+          </div>
+          
         </section>
       </main>
 
